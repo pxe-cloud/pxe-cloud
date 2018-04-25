5421c7e0e0e528b48e87753cad8b18c6b412d354
Néfix Estrada <nefixestrada@gmail.com>
2018-04-25 20:43:00 +0200

    Started with the RethinkDB implementation
    Now the API calls to the DB to get the user and password

--- api/methods/authentication_methods.py
+++ api/methods/authentication_methods.py
@@ -1,12 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Imports
+import rethinkdb as r
+from api.methods.files_methods import read_settings
+from api.methods.rethinkdb_methods import connect
+
 # Check if an user and a password are correct
 def check_auth(args):
-    users = {"user": "1234"}
+    rethink_settings = read_settings("rethinkdb")
+    connect(r)
 
-    # Check if the user exists
-    if args["username"] in users:
+    # Get the user
+    users = r.db(rethink_settings["db_name"]).table("users").filter(r.row["username"] == args["username"]).run()
+
+    # Loop through the cursor to get the user (and check if the user works)
+    for user in users:
 
         # Check if the password is correct
-        if users[args["username"]] == args["password"]:
+        if user["password"] == args["password"]:
             return True
 
     return False
