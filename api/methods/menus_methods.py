93a1dd82b23a1a8e819731de32798c646fcc76a9
Néfix Estrada <nefixestrada@gmail.com>
2018-05-02 19:38:15 +0200

    Added the actions section
    Now you should be able to boot from disk, reboot and shutdown the computer from all the menus

--- api/methods/menus_methods.py
+++ api/methods/menus_methods.py
@@ -67,6 +67,17 @@ def generate_menu(menu_id, username, password, conn):
                 entries_menu += f"initrd {entry['initrd']} || read void\n"
                 entries_menu += "boot || read void\n"
 
+    raw_menu += "item --gap Actions:\n"
+    raw_menu += "item boot_from_disk Boot from disk -->\n"
+    raw_menu += "item reboot Reboot -->\n"
+    raw_menu += "item shutdown Shutdown -->\n"
+    entries_menu += ":boot_from_disk\n"
+    entries_menu += "sanboot --no-describe --drive 0x80\n"
+    entries_menu += ":reboot\n"
+    entries_menu += "reboot\n"
+    entries_menu += ":shutdown\n"
+    entries_menu += "poweroff\n"
+
     raw_menu += "choose target && goto ${target}\n"
     raw_menu += entries_menu
 
