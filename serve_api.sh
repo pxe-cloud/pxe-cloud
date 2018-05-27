26973a20aded8c2dad726a3218d937f13c2e75be
Néfix Estrada <nefixestrada@gmail.com>
2018-05-27 13:16:56 +0200

    Updated the shebang
    Now the Alpine Linux containers can run it aswell

--- serve_api.sh
+++ serve_api.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Start serving the API with uWSGI (reading the IP and the port from the settings file (without logging to avoid displaying the passwords of the users)
 uwsgi --socket $(cat settings.yml | shyaml get-value network.host_ip):$(cat settings.yml | shyaml get-value network.api_port) --protocol=http -w wsgi -L
