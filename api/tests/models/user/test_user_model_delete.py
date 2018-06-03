b654e0988ee665ff78fc939b227c68c213ea1ce0
Néfix Estrada <nefixestrada@gmail.com>
2018-06-04 00:22:37 +0200

    Added tests for the delete method of the user model

---
+++ api/tests/models/user/test_user_model_delete.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class TestUserModelDelete:
+    """
+    Test all the possible responses and errors of the create() method from the User model
+    """
+    def test_delete(self, import_module, user):
+        """
+        Delete a specific user from the DB
+        """
+        User = import_module(path="api.models.user", module="User")
+
+        assert User().get(user["username"]).delete()
